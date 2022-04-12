4419140fae7b17e0aa619e3e174bfaba58c98857
Mitchell Pearsall-Toetz <mitchellpearsall@gmail.com>
2022-04-12 17:54:15 -0500

    Added initial file reading function

--- minimal_todo.ex
+++ minimal_todo.ex
@@ -1,11 +1,18 @@
 defmodule MinimalTodo do
   def start do
-    # ask user for filename
+    filename = IO.gets("Name of .csv to load: ") |> String.trim
+    read(filename)
     # open file and read
     # parse the data
     # ask user for command
     # (read todos, add todos, delete todos, load file, save files)
   end
 
-
+  def read(filename) do
+    case File.read(filename) do
+      {:ok, body}       -> body
+      {:error, reason}  -> IO.puts ~s(Could not open file "#{filename}"\n)
+                           IO.puts ~s("#{:file.format_error reason}"\n)
+    end
+  end
 end
