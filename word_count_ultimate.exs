92d741e79c9475a225357cf32f0386ec5cd7ba54
Mitchell Pearsall-Toetz <mitchellpearsall@gmail.com>
2022-04-12 12:49:37 -0500

    completed functionality and fixed a typo on line 23

--- word_count_ultimate.exs
+++ word_count_ultimate.exs
@@ -16,10 +16,26 @@ somefile.txt -lc
 """
 else
   parts = String.split(filename, "-")
-  filename = List.first(parts) |> String.trim()
+  filename = List.first(parts) |> String.trim
   flags = case Enum.at(parts, 1) do
     # set only "w" flag if none were sent
     nil   -> ["w"]
-    chars -> String.split(chars, "") |> Enum.filter(fn x -> x !+ "" end)
+    chars -> String.split(chars, "") |> Enum.filter(fn x -> x != "" end)
   end
+
+  body = File.read! filename
+  lines = String.split(body, ~r{(\r\n|\n|\r)})
+  words =
+    String.split(body, ~r{(\\n|[^\w'])+})
+    |> Enum.filter(fn x -> x != "" end)
+  chars = String.split(body, "") |> Enum.filter(fn x -> x != "" end)
+
+  Enum.each(flags, fn flag ->
+    case flag do
+      "l" -> IO.puts "Lines: #{Enum.count(lines)}"
+      "w" -> IO.puts "Words: #{Enum.count(words)}"
+      "c" -> IO.puts "Chars: #{Enum.count(chars)}"
+      _   -> nil
+    end
+  end)
 end
