b35ce0aeed08c6374770fff2db25278c407b2ab3
Mitchell Pearsall-Toetz <mitchellpearsall@gmail.com>
2022-04-12 17:28:18 -0500

    Created the starting file minimal_todo, a todo list run through the command line. Also listed basic features

---
+++ minimal_todo.ex
@@ -0,0 +1,11 @@
+defmodule MinimalTodo do
+  def start do
+    # ask user for filename
+    # open file and read
+    # parse the data
+    # ask user for command
+    # (read todos, add todos, delete todos, load file, save files)
+  end
+
+
+end
