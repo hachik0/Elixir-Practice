1eec40ceaaa8ab5716cc1536a9030a72f41a0509
Mitchell Pearsall-Toetz <mitchellpearsall@gmail.com>
2022-04-13 11:47:37 -0500

    Added multiple functions to parse data, map it, and begin adding commands to act on the file data

--- minimal_todo.ex
+++ minimal_todo.ex
@@ -2,10 +2,41 @@ defmodule MinimalTodo do
   def start do
     filename = IO.gets("Name of .csv to load: ") |> String.trim
     read(filename)
-    # open file and read
-    # parse the data
+      |> parse
+      |> get_command
     # ask user for command
     # (read todos, add todos, delete todos, load file, save files)
+  end
+
+  def get_command(data) do
+    prompt = """
+  Type the first letter of the command you want to run
+  R)ead Todos   A)dd a todo   D)elete a Todo  L)oad a .csv    S)ave a .csv"
+  """
+    command = IO.gets(prompt)
+      |> String.trim
+      |> String.downcase
+
+    case command do
+      "r"     -> show_todos(data)
+      "d"     -> delete_todo(data)
+      "q"     -> "Goodbye!"
+      _       -> get_command(data)
+    end
+  end
+
+  def delete_todo(data) do
+    todo = IO.gets("Which todo would you like to delete?\n") |> String.trim
+    if Map.has_key? data, todo do
+      IO.puts "ok."
+      new_map = Map.drop(data, [todo])
+      IO.puts ~s("#{todo}" has been deleted.)
+      get_command(new_map)
+    else
+      IO.puts ~s(There is no Todo named "#{todo}"!)
+      show_todos(data, false)
+      delete_todo(data)
+    end
   end
 
   def read(filename) do
@@ -18,6 +49,30 @@ defmodule MinimalTodo do
   end
 
   def parse(body) do
-    lines = String.split(body, ~r{(\r\n|\n|\r)})
+    [header | lines] = String.split(body, ~r{(\r\n|\n|\r)})
+    titles = tl String.split(header, ",")
+    parse_lines(lines, titles)
+  end
+
+  def parse_lines(lines, titles) do
+    Enum.reduce(lines, %{}, fn line, built ->
+      [name | fields] = String.split(line, ",")
+      if Enum.count(fields) == Enum.count(titles) do
+        line_data = Enum.zip(titles, fields) |> Enum.into(%{})
+        Map.merge(built, %{name => line_data})
+      else
+        built
+      end
+    end)
+  end
+
+  def show_todos(data, next_command? \\ true) do
+    items = Map.keys data
+    IO.puts("You have the following Todos:\n")
+    Enum.each items, fn item -> IO.puts item end
+    IO.puts "\n"
+    if next_command? do
+      get_command(data)
+    end
   end
 end
